3beec3d9dcd9022e43c99778fd05f36d8cc04493
ST10489984 <ST10489984@vcconnect.edu.za>
2025-03-29 22:13:14 +0200

    First commit

--- app/src/main/java/za/co/varsitycollege/st10489984/New_User.kt
+++ app/src/main/java/za/co/varsitycollege/st10489984/New_User.kt
@@ -1,36 +1,20 @@
 package za.co.varsitycollege.st10489984
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class New_User : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_user)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
-
-        val newUserSwitch = findViewById<Switch>(R.id.newUserSwitch)
-
-        getStartedButton.setOnClickListener {
-            var user: Boolean = newUserSwitch.isChecked
-            if (user) {
-                val intent = Intent(this, Home_Page::class.java)
-            } else {
-                val intent = Intent(this,Home_Page::class.java)
-            }
         }
     }
 }
